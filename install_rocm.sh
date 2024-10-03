799c8598538e9cd4a2de0107f610376c7b872513
Mika Laitio <lamikr@gmail.com>
2024-10-02 23:41:52 -0700

    fix the install logic
    Signed-off-by: Mika Laitio <lamikr@gmail.com>

--- install_rocm.sh
+++ install_rocm.sh
@@ -4,6 +4,8 @@ REL_NAME=rocm-6.2.0-14213
 DOC_BASE_DIR_NAME=share/doc/hsa-amd-aqlprofile
 DOC_SRC_DIR_NAME=opt/${REL_NAME}/${DOC_BASE_DIR_NAME}
 LIB_SRC_DIR_NAME=opt/${REL_NAME}/lib
+LIBHSA_AMD_AQLPROFILE64_SYMLINK_BASE_NAME=libhsa-amd-aqlprofile64.so
+LIBHSA_AMD_AQLPROFILE64_FULL_NAME=${LIBHSA_AMD_AQLPROFILE64_SYMLINK_BASE_NAME}.1.0.60200
 
 if [ -z "$1" ]; then
     echo ""
@@ -33,8 +35,8 @@ if [ ! -d ${source_dir_prefix_rocm} ]; then
     exit 3
 fi
 
+cd ${source_dir_prefix_rocm}
 if [ ! -e ${RPM_NAME} ]; then
-    cd ${source_dir_prefix_rocm}
     wget -c ${RPM_URL}
 
     if [ ! -e ${RPM_NAME} ]; then
@@ -43,8 +45,12 @@ if [ ! -e ${RPM_NAME} ]; then
         echo ""
         exit 3
     fi
-    rpm2cpio ${RPM_NAME} | cpio -idmv
 fi
+if [ ! -e ${RPM_NAME} ]; then
+    echo "Could not find: ${source_dir_prefix_rocm}/${RPM_NAME}"
+fi
+echo "Installing: ${source_dir_prefix_rocm}/${RPM_NAME}"
+rpm2cpio ${RPM_NAME} | cpio -idmv
 
 DOC_TRGT_DIR_NAME=${install_dir_prefix_rocm}/${DOC_BASE_DIR_NAME}
 LIB_TRGT_DIR_NAME=${install_dir_prefix_rocm}/lib
@@ -65,9 +71,11 @@ if [ -d ${LIB_SRC_DIR_NAME}/hsa-amd-aqlprofile ]; then
         install -d ${LIB_TRGT_DIR_NAME}/hsa-amd-aqlprofile
     fi
     if [ -d ${LIB_TRGT_DIR_NAME}/hsa-amd-aqlprofile ]; then
-        if [ ! -e ${LIB_TRGT_DIR_NAME}/hsa-amd-aqlprofile/librocprofv2_att.so ]; then
+        if [ ! -e ${LIB_TRGT_DIR_NAME}/${LIBHSA_AMD_AQLPROFILE64_FULL_NAME} ]; then
             install -Dm660 ${source_dir_prefix_rocm}/${LIB_SRC_DIR_NAME}/hsa-amd-aqlprofile/* ${LIB_TRGT_DIR_NAME}/hsa-amd-aqlprofile
-            install -Dm660 ${source_dir_prefix_rocm}/${LIB_SRC_DIR_NAME}/* ${LIB_TRGT_DIR_NAME}
+            install -Dm660 ${source_dir_prefix_rocm}/${LIB_SRC_DIR_NAME}/${LIBHSA_AMD_AQLPROFILE64_FULL_NAME} ${LIB_TRGT_DIR_NAME}
+            ln -s ${LIB_TRGT_DIR_NAME}/${LIBHSA_AMD_AQLPROFILE64_SYMLINK_BASE_NAME}.1 ${LIB_TRGT_DIR_NAME}/${LIBHSA_AMD_AQLPROFILE64_SYMLINK_BASE_NAME}
+            ln -s ${LIB_TRGT_DIR_NAME}/${LIBHSA_AMD_AQLPROFILE64_FULL_NAME} ${LIB_TRGT_DIR_NAME}/${LIBHSA_AMD_AQLPROFILE64_SYMLINK_BASE_NAME}.1
             echo "aql profile libraries installed"
         fi
     fi
